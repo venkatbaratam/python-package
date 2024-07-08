7f6d679fdeb9a7e691168f58ddd40371f5513617
venkat.baratam <AD0041venkat@outlook.com>
2024-07-08 20:05:20 +0530

    structure updated

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME ="python-package.git"
 PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+AUTHOR_USER_NAME = "venkatbaratam"
+AUTHOR_EMAIL = "venkyvenky84990@gmail.com"
 
 setup(
     name=PKG_NAME,
