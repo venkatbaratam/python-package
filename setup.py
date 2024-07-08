65b649f53328d77d5917a6344b312234a4ef9ab0
venkat.baratam <AD0041venkat@outlook.com>
2024-07-08 21:52:11 +0530

    structure updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.4"
 REPO_NAME ="python-package"
-PKG_NAME= "databaseautomation"
+PKG_NAME= "mongodbpkg"
 AUTHOR_USER_NAME = "venkatbaratam"
 AUTHOR_EMAIL = "venkyvenky84990@gmail.com"
 
